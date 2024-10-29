efa1cf5e0fe226ffcf4d2489b837dc81c24bc4dc
Gonzalo Fernandez <165960724+Gonzapp@users.noreply.github.com>
2024-10-29 20:03:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button/Button'
-import ItemListContainer from './components/itemListContainer/itemListContainer'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/Navbar'
 
 
